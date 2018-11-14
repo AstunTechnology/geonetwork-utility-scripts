d0a967bc61f7acc2e40ef8800269b6fb369db911
archaeogeek <jo.k.cook@gmail.com>
2018-11-14 16:06:49 +0000

    Include modules for removing url and adding url, and some error handling. Updated readme to match

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 Scripts for bulk updates of geonetwork based on supplied csv files. Currently includes the following update options:
 
-* Update URL
+* Update URL (replace old URL with new one)
+* Remove URL
+* Add URL
 * Update Sharing Permissions
 
-CAUTION: Work in progress, with absolutely no error handling yet!
+CAUTION: Work in progress, with very minimal error handling!
 
 ## INSTALLATION
 
@@ -23,7 +25,7 @@ CAUTION: Work in progress, with absolutely no error handling yet!
 
 `python geonetwork_update.py --help`
 
-### URL Update
+###urlupdate
 
 `python geonetwork_update.py --url={root geonetwork URL eg http://localhost:8080} --username={admin username} urlupdate`
 * Add password for `{admin username}` at prompt
@@ -32,13 +34,42 @@ CAUTION: Work in progress, with absolutely no error handling yet!
 * Runs `url-host-relocator.xsl` via `/geonetwork/srv/api/0.1/processes/`. This file must be present in the `process` folder for the schema of the records you are trying to update.
 * Requires CSV file in same format as `updateurl.csv.sample`, eg:
   * UUID: uuid of the record
-  * OLDURL: the URL you are wish to update
+  * OLDURL: the URL you wish to update
   * NEWURL: the URL you wish to update to
-  * PROTOCOL: (optional) protocol for the transfer option- NOT YET IMPLEMENTED
-  * NAME: (optional) name for the transfer option- NOT YET IMPLEMENTED
-  * DESCRIPTION: (optional) description for the transfer option- NOT YET IMPLEMENTED
+  * PROTOCOL: (optional) protocol for the transfer option
+  * NAME: (optional) name for the transfer option
+  * DESCRIPTION: (optional) description for the transfer option
 
-### Sharing Update (WIP)
+###urladd
+`python geonetwork_update.py --url={root geonetwork URL eg http://localhost:8080} --username={admin username} urlupdate`
+* Add password for `{admin username}` at prompt
+* Add `filename.csv` at prompt
+
+* Runs `url-host-relocator.xsl` via `/geonetwork/srv/api/0.1/processes/`. This file must be present in the `process` folder for the schema of the records you are trying to update.
+* Requires CSV file in same format as `updateurl.csv.sample`, eg:
+  * UUID: uuid of the record
+  * OLDURL: LEAVE BLANK
+  * NEWURL: the URL you wish add
+  * PROTOCOL: (mandatory) protocol for the new URL
+  * NAME: (mandatory) name for the new URL
+  * DESCRIPTION: (mandatory) description for the new URL
+
+###urlremove
+
+`python geonetwork_update.py --url={root geonetwork URL eg http://localhost:8080} --username={admin username} urlupdate`
+* Add password for `{admin username}` at prompt
+* Add `filename.csv` at prompt
+
+* Runs `/geonetwork/srv/api/0.1/records/batchediting`.
+* Requires CSV file in same format as `updateurl.csv.sample`, eg:
+  * UUID: uuid of the record
+  * OLDURL: the URL you wish to remove
+  * NEWURL: LEAVE BLANK
+  * PROTOCOL: (optional) protocol for the URL
+  * NAME: (mandatory) name for the URL
+  * DESCRIPTION: (optional) description for the URL
+
+### sharing
 
 `python geonetwork_update.py --url={root geonetwork URL eg http://localhost:8080} --username={admin username} sharing`
 * Add password for `{admin username}` at prompt
--- geonetwork_update.py
+++ geonetwork_update.py
@@ -51,27 +51,130 @@ def urlupdate(ctx,csvfile):
 	with open(csvfile, 'rb') as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-			#click.echo(row)
-			params = (
-				('uuids', row['UUID']),
-				('index', 'true'),
-				('urlPrefix', row['OLDURL']),
-				('newUrlPrefix', row['NEWURL'])
-			)
-
-			session = ctx.obj['session']
-			url = ctx.obj['url']
-			session.auth = HTTPBasicAuth(ctx.obj['username'],ctx.obj['password'])
-			headers = session.headers
-			cookies = session.cookies
-			geonetworkUpdateURL = url + '/geonetwork/srv/api/0.1/processes/url-host-relocator'
-			updateURL = session.post(geonetworkUpdateURL, 
-				headers=headers, 
-				params=params, 
-				verify=False
+			if row['OLDURL'] and row['NEWURL']:
+				#click.echo(row)
+				params = (
+					('uuids', row['UUID']),
+					('index', 'true'),
+					('urlPrefix', row['OLDURL']),
+					('newUrlPrefix', row['NEWURL'])
 				)
-			click.echo(updateURL.text)
 
+				session = ctx.obj['session']
+				url = ctx.obj['url']
+				session.auth = HTTPBasicAuth(ctx.obj['username'],ctx.obj['password'])
+				headers = session.headers
+				cookies = session.cookies
+				geonetworkUpdateURL = url + '/geonetwork/srv/api/0.1/processes/url-host-relocator'
+				updateURL = session.post(geonetworkUpdateURL, 
+					headers=headers, 
+					params=params, 
+					verify=False
+					)
+				
+				# get at response for some error handling
+				response = json.loads(updateURL.text)
+
+				if updateURL.status_code == 201 and response["numberOfNullRecords"] == 0 and not response["errors"]:
+					click.echo(click.style(row['UUID'] + ': done', fg='green'))
+				elif updateURL.status_code == 201 and response["numberOfNullRecords"] == 1:
+					click.echo(click.style(row['UUID'] + ': not found', fg='red'))
+				else:
+					click.echo(click.style(row['UUID'] + ': error \n' + updateURL.text, fg='red'))
+			else:
+				click.echo(click.style(row['UUID'] + ': skipped', fg='blue'))
+
+@cli.command()
+@click.option('--csvfile',prompt=True, help='CSV file')
+@click.pass_context
+def urladd(ctx,csvfile):
+
+	# disabling https warnings while testing
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	with open(csvfile, 'rb') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			if not row['OLDURL'] and row['NEWURL']:
+				# build json payload
+				jsonpayload = json.dumps([{"value":"<gmd:onLine xmlns:gmd=\"http://www.isotc211.org/2005/gmd\"> \
+								<gmd:CI_OnlineResource> \
+								<gmd:linkage><gmd:URL>" + row['NEWURL'] + "</gmd:URL></gmd:linkage> \
+								<gmd:protocol><gco:CharacterString xmlns:gco=\"http://www.isotc211.org/2005/gco\">" + row['PROTOCOL'] + "</gco:CharacterString></gmd:protocol>  \
+								<gmd:name><gco:CharacterString xmlns:gco=\"http://www.isotc211.org/2005/gco\">" + row['NAME'] + "</gco:CharacterString></gmd:name>  \
+								<gmd:description><gco:CharacterString xmlns:gco=\"http://www.isotc211.org/2005/gco\">" + row['DESCRIPTION'] + "</gco:CharacterString></gmd:description>  \
+								</gmd:CI_OnlineResource> \
+								</gmd:onLine>",
+								"xpath": "/gmd:MD_Metadata/gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/"}])
+				session = ctx.obj['session']
+				url = ctx.obj['url']
+				session.auth = HTTPBasicAuth(ctx.obj['username'],ctx.obj['password'])
+				headers = session.headers
+				cookies = session.cookies
+				headers.update({'Content-Type': 'application/json'})
+				geonetworkAddURL = url + '/geonetwork/srv/api/0.1/records/batchediting?uuids=' + row['UUID']
+				updateURL = session.put(geonetworkAddURL, 
+					headers=headers, 
+					data=jsonpayload, 
+					verify=False
+					)
+
+				# get at response for some error handling
+				response = json.loads(updateURL.text)
+
+				if updateURL.status_code == 201 and response["numberOfNullRecords"] == 0 and not response["errors"]:
+					click.echo(click.style(row['UUID'] + ': done', fg='green'))
+				elif updateURL.status_code == 201 and response["numberOfNullRecords"] == 1:
+					click.echo(click.style(row['UUID'] + ': not found', fg='red'))
+				else:
+					click.echo(click.style(row['UUID'] + ': error \n' + updateURL.text, fg='red'))
+			else:
+				click.echo(click.style(row['UUID'] + ': skipped', fg='blue'))
+
+
+@cli.command()
+@click.option('--csvfile',prompt=True, help='CSV file')
+@click.pass_context
+def urlremove(ctx,csvfile):
+
+	# disabling https warnings while testing
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	with open(csvfile, 'rb') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			if row['OLDURL'] and not row['NEWURL']:
+				#click.echo(row)
+				params = (
+					('uuids', row['UUID']),
+					('index', 'true'),
+					('url', row['OLDURL']),
+					('name', row['NAME'])
+				)
+
+				session = ctx.obj['session']
+				url = ctx.obj['url']
+				session.auth = HTTPBasicAuth(ctx.obj['username'],ctx.obj['password'])
+				headers = session.headers
+				cookies = session.cookies
+				geonetworkUpdateURL = url + '/geonetwork/srv/api/0.1/processes/onlinesrc-remove'
+				updateURL = session.post(geonetworkUpdateURL, 
+					headers=headers, 
+					params=params, 
+					verify=False
+					)
+				
+				# get at response for some error handling
+				response = json.loads(updateURL.text)
+
+				if updateURL.status_code == 201 and response["numberOfNullRecords"] == 0 and not response["errors"]:
+					click.echo(click.style(row['UUID'] + ': done', fg='green'))
+				elif updateURL.status_code == 201 and response["numberOfNullRecords"] == 1:
+					click.echo(click.style(row['UUID'] + ': not found', fg='red'))
+				else:
+					click.echo(click.style(row['UUID'] + ': error \n' + updateURL.text, fg='red'))
+			else:
+				click.echo(click.style(row['UUID'] + ': skipped', fg='blue'))
 
 @cli.command()
 @click.option('--csvfile',prompt=True, help='CSV file')
@@ -140,7 +243,13 @@ def sharing(ctx,csvfile):
 			verify=False,
 			json = privdict
 			)
-		click.echo(sharingURL.status_code)
+		# get at response for some error handling
+		response = json.loads(updateURL.text)
+
+		if sharingURL.status_code == 201:
+			click.echo(click.style(row['UUID'] + ': done', fg='green'))
+		else:
+			click.echo(click.style(row['UUID'] + ': error \n' + updateURL.text, fg='red'))
 
 
 if __name__ == '__main__':
