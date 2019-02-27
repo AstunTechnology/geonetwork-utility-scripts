3dacc751da10dca20665baf74b52093bdb3c1026
archaeogeek <jo.k.cook@gmail.com>
2019-02-27 18:35:02 +0000

    Updated pip requirements, made changes to email updates scripts and sql

--- email_update.py
+++ email_update.py
@@ -109,16 +109,73 @@ def updateuser(ctx,csvfile):
 		with conn:
 			with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
 				user_sql = slurp(os.path.join(base_dir, os.path.join('sql','get_userid_for_email.sql')))
-				logger.info('Getting user_ids for email: ' + row['email'])
-				cur.execute(user_sql, {'email': row['email']})
+				logger.info('Getting user_ids for email: ' + row['OLDEMAIL'])
+				cur.execute(user_sql, {'email': row['OLDEMAIL']})
 				logger.debug('Executed get_userid_for_email.sql:\n%s\n%s' % (cur.query, cur.statusmessage))
 				user_id = cur.fetchall()
 				if user_id:
-					logger.info('email: %s has id %s' % (row['email'], user_id))
+					logger.info('email: %s has id %s' % (row['OLDEMAIL'], user_id))
 				else:
-					logger.info('email: %s is not a login email' % row['email'])
+					logger.info('email: %s is not a login email' % row['OLDEMAIL'])
 
 				# update email address for given user_id
+				for u in user_id:
+					updateuser_sql = slurp(os.path.join(base_dir, os.path.join('sql', 'update_email_for_userid.sql')))
+					logger.info('Updating email for user_id: %s' % (u))
+					cur.execute(updateuser_sql, {'user_id': u, 'email': row['NEWEMAIL']})
+	logger.info('Finished updating user emails')
+
+@cli.command()
+@click.option('--csvfile',prompt=True, help='CSV file')
+@click.pass_context
+def updatemetadata(ctx,csvfile):
+	"""Update email addresses in metadata, passed from a CSV file"""
+
+	logger.info("started updating emails in metadata")
+
+	# disabling https warnings while testing
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+	session = ctx.obj['session']
+	url = ctx.obj['url']
+	session.auth = HTTPBasicAuth(ctx.obj['username'],ctx.obj['password'])
+	headers = session.headers
+	cookies = session.cookies
+
+ 	uuidlist = []
+
+	# read oldemails from csv and pop selected records into a bucket
+	df = pd.read_csv(csvfile)
+	for index, row in df.iterrows():
+		geonetworkSearchURL = url + '/eng/q?any=' + row['OLDEMAIL']
+		click.echo(geonetworkSearchURL)
+		searchURL = session.put(geonetworkSearchURL,
+			headers=headers,
+			verify=False
+			)
+
+		# add search results to a bucket called metadata
+		geonetworkSelectURL = url + '/api/0.1/selections/metadata'
+		selectURL = session.put(geonetworkSelectURL,
+			headers=headers,
+			verify=False
+			)
+
+		# construct params for sending to email-replacer process
+		params = (
+				('oldEmail', row['OLDEMAIL']),
+				('newEmail', row['NEWEMAIL']),
+				('bucket', 'metadata')
+			)
+
+		geonetworkEmailUpdate = url + '/api/0.1/processes/email-replacer'
+		updateURL = session.post(geonetworkEmailUpdate,
+				headers=headers,
+				params=params,
+				verify=False
+			)
+
+		click.echo(updateURL.text)
 
 
 
--- sql/update_email_for_userid.sql
+++ sql/update_email_for_userid.sql
@@ -1,3 +1,3 @@
 -- Update emails for given user_ids
 
-UPDATE email SET email = %(email)s WHERE user_id = ANY(%(emaillist)s)
+UPDATE email SET email = %(email)s WHERE user_id = ANY(%(user_id)s)
