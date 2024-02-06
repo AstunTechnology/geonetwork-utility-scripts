728ac4b5392f3759d1d98bc722dd2939168b3be9
Elena Robu <elenarobu@astuntechnology.com>
2024-02-06 16:14:19 +0000

    update email script, include sample and documentation

--- README.md
+++ README.md
@@ -10,11 +10,16 @@ A set of scripts for bulk updates of geonetwork based on supplied csv files. Cur
 
 ### Python
 
-The Python package `psycopg2` requires the system package `libpq-dev` to be installed.
+The Python package `psycopg2` requires the following system packages to be installed:
+
+* `libpq-dev`
+* `build-dep`
+* `python-psycopg2`
+
 **Installed with apt or the package manager of your system**
 
 ```
-    sudo apt-get install libpq-dev
+    sudo apt-get install libpq-dev build-dep python-psycopg2
 ```
 
 # INSTALLATION
@@ -41,24 +46,38 @@ Output results to `report.csv`- a list of the UUIDs, URLs and their error (or su
 
 A sample file is provided in the `samples` directory which can be used to test the script.
 
+## email_update.py
 
+Module for updating a user's email address either in the user's GeoNetwork profile or in metadata records.
+
+### updating user's email address in the user's profile
+
+`python email_update.py updateuser`
+
+### updating user's email address in metadata records
+
+`python email_update.py updatemetadata`
+
+#### Requirements (common to both scripts)
+
+The scripts require a CSV file with 2 columns (called `old_email` and `new_email`)- see `./samples/email_update.csv.sample` for an example
+
+When ran, the scripts will prompt for the following:
+
+* GeoNetwork URL as far as the node name: e.g. `http://localhost:8080/geonetwork/srv`
+* GeoNetwork username: this needs to be a user with the relevant permissions (e.g. an Administrator)
+* GeoNetwork password: the login password of the user- this will be required twice for confirmation (the input is hidden for security)
+* CSV file path: this is the FULL path to the CSV file containing the updates (see `./samples/email_update.csv.sample` for structure)
+
+The logs will be appended to `email-update.log`.
 
 **######## TO BE REVISED (Don't read below here)########**
-
-# Tests
-
-IN PROGRESS
-
-**Requires python-nose**
-
-Run `nosetests` in the root folder to execute all tests in the `tests` folder.
-
 
 # Metadata Update Reminder
 
 See https://astuntech.atlassian.net/wiki/spaces/SYSADMIN/pages/140763762/Metadata+Update+Frequency+Queries for more details
 
-Requirements:
+## Requirements
 
 > Send emails when metadata is due for an update or overdue an update, based on the supplied update frequency, date updated, and point of contact
 
@@ -67,6 +86,7 @@ Requirements:
 ### Database
 
 Once the dependencies are installed run `setup.sql` against the GeoNetwork database.
+
 ### Logging
 
 All log messages are written to `stdout`, should output be written to a file a sample logrotate configuration is provided (`updatereminder.logrotate`) which should be copied to `/etc/logrotate.d/`.
@@ -75,15 +95,4 @@ All log messages are written to `stdout`, should output be written to a file a s
 
 For usage run `updatereminder.py` using the `python` interpreter from the virtual environment:
 
-    python updatereminder.py --help
-
-## Tests
-
-Not done yet
-
-## Implementation
-
-### Workflow
-
-* The script `updatereminder.py` is ran daily, it:
-    * does some stuff
+`python updatereminder.py --help`
--- email_update.py
+++ email_update.py
@@ -3,7 +3,6 @@ import click
 from requests.auth import HTTPBasicAuth
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import json
-import xml.etree.ElementTree
 import logging
 import psycopg2
 import psycopg2.extras
@@ -14,14 +13,14 @@ from collections import defaultdict
 
 logger = logging.getLogger(__name__)
 
-loggingHandler = RotatingFileHandler("email-update.log",
+loggingHandler = RotatingFileHandler('email-update.log',
 									  maxBytes=102400, backupCount=5)
 loggingHandler.setLevel(logging.DEBUG)
 logger.setLevel(logging.DEBUG)
 
 loggingHandler.setFormatter(
-	logging.Formatter("%(asctime)s - "
-					  "%(levelname)s - %(message)s"))
+	logging.Formatter('%(asctime)s - '
+					  '%(levelname)s - %(message)s'))
 logger.addHandler(loggingHandler)
 
 
@@ -34,18 +33,18 @@ def parse_config(conf_file):
 	with open(conf_file) as props:
 		for line in props:
 			if line.startswith('jdbc'):
-				k, v = line.split("=")
+				k, v = line.split('=')
 				d[k] = v.rstrip('\n')
 	return d
 
 
 def build_conn_args(config):
 	return {
-		'database': config.get("jdbc.database"),
-		'host': config.get("jdbc.host"),
-		'port': config.get("jdbc.port"),
-		'user': config.get("jdbc.username"),
-		'password': config.get("jdbc.password")
+		'database': config.get('jdbc.database'),
+		'host': config.get('jdbc.host'),
+		'port': config.get('jdbc.port'),
+		'user': config.get('jdbc.username'),
+		'password': config.get('jdbc.password')
 	}
 
 def slurp(path):
@@ -53,9 +52,9 @@ def slurp(path):
 		return f.read()
 
 @click.group()
-@click.option('--url', required=True, help='Geonetwork URL as far as the node name')
-@click.option('--username', required=True, help='Geonetwork username')
-@click.option('--password', required=True, help='Geonetwork password')
+@click.option('--url', required=True, prompt='GeoNetwork URL as far as the node name', help='GeoNetwork URL as far as the node name')
+@click.option('--username', required=True, prompt='GeoNetwork username', help='GeoNetwork username')
+@click.option('--password', required=True, prompt='GeoNetwork password', help='GeoNetwork password', hide_input=True, confirmation_prompt=True)
 @click.pass_context
 def cli(ctx,url,username,password):
 	"""Module for updating email addresses in geonetwork user details and metadata records see readme for further details"""
@@ -69,10 +68,12 @@ def cli(ctx,url,username,password):
 
 	# Make a call to an endpoint to get cookies and an xsrf token
 
-	geonetwork_url = url + '/eng/info?type=me'
-	r_post = geonetwork_session.post(geonetwork_url,
-		verify=False
-		)
+	geonetwork_user_info_endpoint = url + '/api/me'
+	get_user_info_response = geonetwork_session.get(geonetwork_user_info_endpoint, verify=False)
+
+	if get_user_info_response != 200:
+		logger.error('Authentication to the portal failed.')
+		raise Exception ('Failed to authenticate. Try again!')
 
 	token = geonetwork_session.cookies.get('XSRF-TOKEN')
 	geonetwork_session.headers.update({"X-XSRF-TOKEN" : geonetwork_session.cookies.get('XSRF-TOKEN')})
@@ -90,17 +91,17 @@ def cli(ctx,url,username,password):
 	}
 
 @cli.command()
-@click.option('--csvfile',prompt=True, help='CSV file')
+@click.option('--csvfile', required=True, prompt='CSV file to use for update', help='CSV file to use for update')
 @click.pass_context
 def updateuser(ctx,csvfile):
 	"""Update email address in user details, passed from a CSV file"""
 
-	logger.info("update user details started")
+	logger.info('Started updating user details')
 
 	conn = psycopg2.connect(**ctx.obj['db_conn_args'])
 	base_dir = os.path.dirname(os.path.realpath(__file__))
 
-	# disabling https warnings while testing
+	# disabling https warnings for testing purposes
 	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 	# get user id for given email
@@ -108,32 +109,37 @@ def updateuser(ctx,csvfile):
 	for index, row in df.iterrows():
 		with conn:
 			with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
 				user_sql = slurp(os.path.join(base_dir, os.path.join('sql','get_userid_for_email.sql')))
-				logger.info('Getting user_ids for email: ' + row['OLDEMAIL'])
-				cur.execute(user_sql, {'email': row['OLDEMAIL']})
-				logger.debug('Executed get_userid_for_email.sql:\n%s\n%s' % (cur.query, cur.statusmessage))
+				logger.info(f'Getting user ID for email: {row["old_email"]}')
+
+				cur.execute(user_sql, {'email': row['old_email']})
+				logger.debug(f'Executed get_userid_for_email.sql:{cur.query}{cur.statusmessage}')
+
 				user_id = cur.fetchall()
 				if user_id:
-					logger.info('email: %s has id %s' % (row['OLDEMAIL'], user_id))
+					logger.info(f'Email {row["old_email"]} has ID {user_id}')
+
 				else:
-					logger.info('email: %s is not a login email' % row['OLDEMAIL'])
+					logger.error(f'Email {row["old_email"]} is not a login email')
 
 				# update email address for given user_id
 				for u in user_id:
 					updateuser_sql = slurp(os.path.join(base_dir, os.path.join('sql', 'update_email_for_userid.sql')))
-					logger.info('Updating email for user_id: %s' % (u))
-					cur.execute(updateuser_sql, {'user_id': u, 'email': row['NEWEMAIL']})
+					logger.info(f'Updating email for user ID: {u}')
+
+					cur.execute(updateuser_sql, {'user_id': u, 'email': row['new_email']})
 	logger.info('Finished updating user emails')
 
 @cli.command()
-@click.option('--csvfile',prompt=True, help='CSV file')
+@click.option('--csvfile', required=True, prompt='CSV file to use for update', help='CSV file to use for update')
 @click.pass_context
 def updatemetadata(ctx,csvfile):
 	"""Update email addresses in metadata, passed from a CSV file"""
 
-	logger.info("started updating emails in metadata")
+	logger.info('Started updating emails in metadata')
 
-	# disabling https warnings while testing
+	# disabling https warnings for testing purposes
 	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 	session = ctx.obj['session']
@@ -142,44 +148,122 @@ def updatemetadata(ctx,csvfile):
 	headers = session.headers
 	cookies = session.cookies
 
-	uuidlist = []
+	geonetwork_search_endpoint = url + '/api/search/records/_search'
+
+	uuid_list = []
 
 	# read oldemails from csv and pop selected records into a bucket
 	df = pd.read_csv(csvfile)
+
 	for index, row in df.iterrows():
-		geonetworkSearchURL = url + '/eng/q?_isTemplate=y+or+n&any=' + row['OLDEMAIL']
-		click.echo(geonetworkSearchURL)
-		searchURL = session.put(geonetworkSearchURL,
-			headers=headers,
-			verify=False
-			)
 
-		# add search results to a bucket called metadata
-		geonetworkSelectURL = url + '/api/0.1/selections/metadata'
-		selectURL = session.put(geonetworkSelectURL,
-			headers=headers,
-			verify=False
-			)
+		try:
+			headers.update({'Content-Type': 'application/json'})
 
-		# construct params for sending to email-replacer process
-		params = (
-				('oldEmail', row['OLDEMAIL']),
-				('newEmail', row['NEWEMAIL']),
-				('bucket', 'metadata')
-			)
-
-		geonetworkEmailUpdate = url + '/api/0.1/processes/email-replacer'
-		updateURL = session.post(geonetworkEmailUpdate,
+			find_records_response = session.post(
+				geonetwork_search_endpoint,
 				headers=headers,
-				params=params,
+				json=getAPIQuery(row['old_email']),
 				verify=False
 			)
-		logger.info('Updating metadata for email: %s' % (row['OLDEMAIL']))
 
-	logger.info('Finished updating user emails')
+			if find_records_response.status_code not in (200, 201):
+				logger.error(f'Unexpected status code {find_records_response.status_code} - Email ({row["old_email"]}) couldn\'t be found')
+				return None
+
+			find_records_json_response = find_records_response.json()
+
+			for record in find_records_json_response['hits']['hits']:
+				for point_of_contact in record['_source'].get('contact', []):
+					if row['old_email'] == point_of_contact['email']:
+						if record['_source']['uuid'] not in uuid_list:
+							uuid_list.append(record['_source']['uuid'])
+
+				for contact_for_resource in record['_source'].get('contactForResource', []):
+					if row['old_email'] == contact_for_resource['email']:
+						if record['_source']['uuid'] not in uuid_list:
+							uuid_list.append(record['_source']['uuid'])
+
+		except Exception as e:
+			logger.error(f'Exception raised for email {row["old_email"]}: {e}')
+
+		if len(uuid_list) == 0:
+			print(f'\n No records found containing email "{row["old_email"]}" - Nothing to update.')
+			print(30 * '-')
+			logger.warning(f'No records found containing email {row["old_email"]}')
+			continue
+
+		else:
+			print(f'\n Records updated for email "{row["old_email"]}": {uuid_list}')
+
+		create_selection_params={
+			'uuid': uuid_list
+		}
+
+		# add search results to a bucket called metadata
+		geonetwork_selections_endpoint = url + '/api/selections/metadata'
+		build_selection_response = session.put(geonetwork_selections_endpoint,
+			headers=headers,
+			verify=False,
+			params=create_selection_params
+		)
+
+		if build_selection_response.status_code not in (200, 201):
+				logger.error(f'Unexpected status code {build_selection_response.status_code}: Could not build the selection - Aborting.')
+				raise Exception(f'Unexpected API response:\n{json.dumps(build_selection_response.json(), indent=2)}')
+
+		# check records selection
+		geonetwork_selections_endpoint = url + '/api/selections/metadata'
+		get_current_selection_response = session.get(geonetwork_selections_endpoint,
+			headers=headers,
+			verify=False
+		)
+
+		logger.info(f'Selected UUIDs: {get_current_selection_response.json()}')
+
+		# construct params for sending to email-replacer process
+		params = {
+			'oldEmail': row['old_email'],
+			'newEmail': row['new_email'],
+			'bucket': 'metadata'
+		}
+
+		geonetwork_processing_endpoint = url + '/api/processes/email-replacer'
+
+		geonetwork_processing_response = session.post(geonetwork_processing_endpoint,
+			headers=headers,
+			params=params,
+			verify=False
+		)
+
+		geonetwork_processing_json_response = geonetwork_processing_response.json()
+
+		logger.info(f'Updating metadata for email: {row["old_email"]}')
+
+		if geonetwork_processing_response.status_code not in (200, 201):
+			logger.error(f'Unexpected status code {geonetwork_processing_response.status_code}: Could not update metadata.')
+			raise Exception(f'Unexpected API response:\n{json.dumps(geonetwork_processing_json_response, indent=2)}')
+
+		print('\n' + json.dumps(geonetwork_processing_json_response, indent=2))
+		print(30 * '-')
+		logger.info('Finished updating metadata records')
 
 
-
+def getAPIQuery(email):
+	return {
+    "query": {
+        "query_string": {
+            "query": email
+        }
+    },
+    "_source": [
+        "resourceTitle*",
+        "metadataIdentifier",
+        "uuid",
+        "contact*",
+        "contactForResource*"
+    ]
+}
 
 
 if __name__ == '__main__':
